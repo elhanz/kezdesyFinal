ad9014d2aa3d6972e4d154fdecdc5224876a843d
elhanz <elhanz@mail.ru>
2022-05-21 23:14:00 +0600

    all rooms

--- app/src/main/java/com/example/kezdesy/MainActivity.kt
+++ app/src/main/java/com/example/kezdesy/MainActivity.kt
@@ -1,22 +1,74 @@
 package com.example.kezdesy
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Context
+import android.content.SharedPreferences
 import android.os.Bundle
-import androidx.navigation.Navigation.findNavController
-import androidx.navigation.findNavController
-import androidx.navigation.ui.setupWithNavController
-import com.google.android.material.bottomnavigation.BottomNavigationView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import app.com.retrofitwithrecyclerviewkotlin.RecyclerAdapter
+import com.example.kezdesy.api.ApiConfig
+import com.example.kezdesy.model.Room
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var recyclerView: RecyclerView
+    lateinit var recyclerAdapter: RecyclerAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        recyclerView = findViewById(R.id.recyclerView)
+        recyclerAdapter = RecyclerAdapter(this)
+        recyclerView.layoutManager = LinearLayoutManager(this)
+        recyclerView.adapter = recyclerAdapter
 
 
+        val apiUser = ApiConfig.API_USER
+        apiUser.getRooms().enqueue(
+            object : Callback<List<Room>> {
 
-        //Initialize the bottom navigation view
-        //create bottom navigation view object
+
+                override fun onResponse(call: Call<List<Room>>, response: Response<List<Room>>) {
+                    if (response.isSuccessful) {
+                        Toast.makeText(
+                            this@MainActivity,
+                            "There are our rooms",
+                            Toast.LENGTH_LONG
+                        ).show()
+                        if (response?.body() != null)
+                            recyclerAdapter.setMovieListItems(response.body()!!)
+
+                    } else {
+                        Toast.makeText(
+                            this@MainActivity,
+                            "Rooms Failed 1 ",
+                            Toast.LENGTH_LONG
+                        ).show()
+                    }
+                }
+
+                override fun onFailure(call: Call<List<Room>>, t: Throwable) {
+                    Toast.makeText(
+                        this@MainActivity,
+                        "Rooms Failed 2",
+                        Toast.LENGTH_LONG
+                    ).show()
+                }
+            }
+        )
+    }
+}
+
+
+//Initialize the bottom navigation view
+//create bottom navigation view object
 //        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottom_navigatin_view)
 //        val navController = findNavController(R.id.nav_fragment)
 //        bottomNavigationView.setupWithNavController(navController)
@@ -26,5 +78,3 @@ class MainActivity : AppCompatActivity() {
 
 
 
-    }
-}
---
+++ app/src/main/java/com/example/kezdesy/adapter/RecyclerAdapter.kt
@@ -0,0 +1,51 @@
+package app.com.retrofitwithrecyclerviewkotlin
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.kezdesy.R
+import com.example.kezdesy.model.Room
+
+class RecyclerAdapter(val context: Context) : RecyclerView.Adapter<RecyclerAdapter.MyViewHolder>() {
+
+    var myList: List<Room> = listOf()
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
+
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.row_layout, parent, false)
+        return MyViewHolder(view)
+    }
+
+    override fun getItemCount(): Int {
+        return myList.size
+    }
+
+    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+
+        holder.header.text = myList.get(position).header
+        holder.description.text = myList.get(position).description
+        holder.minAge.text = myList.get(position).minAgeLimit.toString()
+        holder.maxAge.text = myList.get(position).maxAgeLimit.toString()
+        holder.maxMembers.text = myList.get(position).maxMembers.toString()
+        holder.city.text = myList.get(position).city
+
+    }
+
+    fun setMovieListItems(roomList: List<Room>) {
+        this.myList = roomList;
+        notifyDataSetChanged()
+    }
+
+    class MyViewHolder(itemView: View?) : RecyclerView.ViewHolder(itemView!!) {
+        val header: TextView = itemView!!.findViewById(R.id.header_txt)
+        val description: TextView = itemView!!.findViewById(R.id.description_txt)
+        val minAge: TextView = itemView!!.findViewById(R.id.minAge_txt)
+        val maxAge: TextView = itemView!!.findViewById(R.id.maxAge_txt)
+        val maxMembers: TextView = itemView!!.findViewById(R.id.maxMembers_txt)
+        val city: TextView = itemView!!.findViewById(R.id.city_txt)
+
+    }
+}
